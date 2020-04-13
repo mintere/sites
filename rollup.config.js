e9dbdcf92ccf85226bd8d27649ee7a57258ba1cf
Ben Aubin <ben@benaubin.com>
2020-04-13 18:06:40 -0500

    init commit

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+import pkg from './package.json';
+
+export default [
+	{
+		input: 'src/main.js',
+		external: ['ms'],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+];
