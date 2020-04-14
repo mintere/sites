68d3650496b8cb9e620d50a28fc007d0f7d0c037
Ben Aubin <ben@mintere.com>
2020-04-13 19:02:25 -0500

    build script & docs

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,42 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+
 import pkg from './package.json';
-import pkg from './package.json';
+
+import typescript from '@wessberg/rollup-plugin-ts';
+import alias from "@rollup/plugin-alias";
 
 export default [
 	{
-		input: 'src/main.js',
-		external: ['ms'],
-		output: [
-			{ file: pkg.main, format: 'cjs' },
-			{ file: pkg.module, format: 'es' }
+		input: 'src/index.ts',
+		output: {
+			name: 'mintere-sites-renderer',
+			file: pkg.browser,
+			format: 'umd'
+		},
+		plugins: [
+      typescript(),
+			alias({
+				entries: [
+					{ find: 'handlebars', replacement: 'node_modules/handlebars/dist/handlebars.min.js' }
+				],
+			}),
+			resolve({browser: true}),
+			commonjs(),
 		]
+	},
+	{
+		input: 'src/index.ts',
+		external: [
+			"handlebars",
+			"fs"
+		],
+		output: [
+			{ dir: "dist", format: 'cjs' },
+			{ dir: "dist", format: 'es' }
+    ],
+    plugins: [
+			typescript()
+    ]
 	}
 ];
